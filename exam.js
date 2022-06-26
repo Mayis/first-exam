3bd98fcbd195ae9641aaf4421f7ae7ea54e2015e
Mayis <saroyan.mayis.ait@gmail.com>
2022-06-26 14:25:15 +0400

    my first exam

---
+++ exam.js
@@ -0,0 +1,477 @@
+// 1 Թվարկել բոլոր թայփերը մեկնաբանելով և սահմանելով յուրաքանչյուրի աշխատանքը (բացառությամբ Symbol)
+/* type -ery linum en 2 tesaki primitiv ev kompleqs kam object type: 
+primitive type---
+Number--bolor tvery Js um patkanum en number typin,,nayev NaN y patkanum e number typein
+
+string-- "", '',``, nman chakertneri mej grvacnery string en ,, himnakanum texter, bayc karox enq grel texti mej tver,
+            bayc matematikakan gorcoxutyunner katarelu hamar petq e darcnenq tiv:
+
+boolean-- true kam false, 
+null -- boolean arjeqy false,inqn irenov nshanakum e datarkutyun ,, bayc hatkanshakan e vor ete shat usumnasirenq,
+        ktesnenq vor null-y nuynpes object e,kareli e handipel API harcumer katarelis, erb vor aknkalvum e stanal object,
+        bayc stanum enq datarkutyun aysinq null:
+
+undefined--stanum e ayn popoxakane vory haytararvel e,, bayc arjeq chi stacel,aysinqn mi ban vory ka bayc arjeq chuni:
+
+symbol--
+bigInt--ashxatanq mec tveri het:
+
+
+object type--  sranc bolori himqum ynkac e object:
+object
+array
+function
+date
+regExp
+error
+infinity
+NaN
+*/
+
+// 2 Ինչ տարբերություններ կան LET-ի, CONST-ի և VAR-ի միջև
+/* 
+var ov ev let ov haytararvac popoxakanery kareli e popoxel, bayc constiny voch:
+var -y ashxatum e hoisting ov ,isk let -y voch:
+let-y ev consty-y blokskop en,, aysinqn mtnum en bloki mej ,,bayc blookic durs chen dalis:
+var-y function scope e, ashxatum e funcion scopi mej cankacac vayrum:
+var-ov karonq enq haytararel nuyn anunov miqani popoxakan,bayc kashxati miayn verjine,isk letov 
+    nuyn anunov miqani popoxakan haytararel chenq karox:
+let ov ev const ov hayararvacnery ashxatum en miayn stexcumic heto:
+let ev const-y  iskzbane chen exel, avelacel en ES6-ic heto:
+*/
+
+// 3 Թվարկել տեսանելիության դաշտերը և սահմանել իրենց տարբերությունները
+// 4 Ի՞նչ տարբերություն կա Block Scope-ի և Function scope-ի միջև
+/* 
+ka 3 tesaneliutyan dashter
+Block scope
+function scope
+global scope
+
+minchev ES6 y , JS um kar miayn function scope  ev global scope
+let i ev var i avelanaluc heto avelacav nayev block scope,aysinqn 
+popoxakane vory haytararvel e {} i mej chi karox ogtagorcvel drsum:
+popoxakane vory haytararvel e function i nersum hamarvum e local ayd functioni hamer,
+aysinqn karox e ogtagorcvel miayn dra nersum, sa nyun e ev var-i,ev let-i,ev const-i hamar:
+nuyn anunov popoxakaner menq karox enq hayrarel tarber functionnerum: 
+
+*/
+
+// 5 Ինչո՞վ են վտանգավոր գլոբալ փոփոխականներն ու var-երը
+
+/* 
+gloabal popoxakanery ev var-y vtangavor en qani vor var-ov karox enq haytararel golbal popoxakan,
+ev heto moranal dra masin, ev eli nuyn anunov stexcel evs meky, ayd depqum verevum stexcvace chi 
+ashxati, dra hamar lav e stexcel function scope um, baci ayd JS y aveli shat jamanak e caxsum 
+gtnelu hamar global popoxakanery qanc te local haytararvac popoxakanery:
+*/
+
+// 6  Թվարկել ֆունկցիաները ըստ իրենց անունների ու մեկնաբանել իրենց աշխատանքը
+/* 
+function expression
+function declaration
+ + (arrow function)
+
+ function decleration y karox e kanchvel canakacac vayrum, ayspes asac JS y tesnum decleraton functionnery stexcman
+ pahic ev arajiny kardum dranq , hanelov verev, isk function expresionnery qani vor veragrvac en popoxakannerin,
+ karox en ogagorcvel miayn stexcvleuc heto, ev JS y chi kardum dranq ayn pahin ev interpritatory hasnum e tvyal toxin:
+function expressionnery stexcvum en aypes asac ananun,aysiqn nranc kanchelu hamar ogtagorcvum e ayn popoxakani anuny,
+vorin vor veragrvel e functiony:
+*/
+
+// 7 ինչ տարբերություն կա postfix և prefix գրելաձևի միջև, ու որ դեպքերում ենք իրենց օգտագործում
+/* 
+sytax postfix  i++;
+      prefix ++i;
+himnakanum ogtagoecum enq looperum,ashxatanqe mek qaylov araj ancnelu hamar,
+qani vor ++y avelacnum e 1 , i++;tpum e ev heto nor avelacnum, ++i,avelacnum e nor tpum:
+
+*/
+//8 Մեկնաբանել null-ի և undefined-ի տարբերությունները
+/* 
+undefined nshanakum e vor popoxakany haytararvac e bayc chuni arjeq, kam der chi stacel arjeq,
+minch der null-y datarkutun e, ayn paste vor type of null veradarcnum e object, da ES i bugeric e:
+null ==undefined ,klini ture, qani vor erkusnel hamarvum en boolean false arjeq:
+*/
+
+//9 HTML-ում script թեգը միացնելու ամենահարմար վայրը դա ... շարունակել միտքը և սահմանել թե ինչու
+/* 
+body-i pakvox tag-ic araj, qani vor domi het ashxatanqi jamanak , karox e xndirner tal, dra hamar chisht e
+vopreszi arajiny karda html y ev heto miayn JS-y, ayd depqum aranc xndri karox enq ashxatel DOM i het:
+*/
+
+// 10 JavaScript-ը արդյո՞ք կարող է բրաուզերում աշխատել առանց HTML-ի, գրեք այո կամ ոչ // voch:
+
+// 11 թվարկեք Boolean օպերատորները և սահմանեք նրանց նշանակություններն ու անունները
+// 12 ինչ տարբերություն կա այս օպերատոների միջև ==, ===
+/* 
+== havasar e miayn arjeqov:
+===havasar e ev arjeqov ev typov:
+!= havasar che
+!==havasar che kam arjeqov kam typeov
+> mec e
+<poqr e
+>= mec kam havasar e
+<= poqr kam havasar e
+&& ev ..ev ,,,veradarcnum e arajin false arjeqy kam verjin true arjeqy:
+|| kam..kam ,, veradarcnum e arajin true kam verjin false arjeqy:
+&& i || i mej arajnaherdutyune trvum e && in:
+
+! hakadarc
+
+ternory
+ payman ? true:false
+*/
+
+// 13 ինչ տարբերություն կա պրիմիտիվ և հղումային թայփերի միջև
+/* 
+primitiv typery chunen methodner, qani vor nranq object chen, baci stringic, qani vor JS-y stringy
+veracum e string object i ev talis hnaravorutyun asxhatelu methodnerov:
+
+primitiv typery copy anelis ev heto arjeqy poxelis xndirner chi arajanum,qani vor nranq pahvum en tarber stack-erum,
+ev voch mi kap chunen irar het:
+hxumayin kam object typery aveli dinamik en, asyiqn chunen fixvac chap,nrancic shatery hamarvum en object,ev
+hetevabar unen nayev methodner:
+hxumayin tipery xndirner unen copy anelis qani vor nranq hxum en nuyn objection vory pahvum e heap um,aysiqn nranc
+anunnery pahpanvum en stack um bayc arjeqy vorin nranq hxum en pahpanvum e heap um, ev popoxutyan depqum poxvum e
+bolor texerum , qani vor nranq nxum en nuyn objectin:
+*/
+
+// 14 ինչի՞ համար են օգտագործվում BREAK և CONTINUE օպերատորները և ո՞րտեղ
+/* 
+break durs gal ciklic,
+cantinue - bac toxel tvyal pahy cilki mej:
+break ov karox enq durs gal nayev block i ashxatanqic:
+*/
+
+// 15 Ո՞ր մաթեմատիկական օպերատորի դեպքում և ո՞ր պագայաում console-ում կարող ենք ստանալ infinity:
+/*
+tivy bajanelov 0 i vra stanum enq infinity 
+orinak 100/0 = infinity
+*/
+
+// 16 ի՞նչ է նշանակում NaN և ո՞ր դեպքում կարող ենք իրեն ստանալ
+/* 
+NaN not a number
+tvin avelacnelov aynipi ban vory tiv che stanum enq NaN :
+NaN i type number e:
+*/
+
+// 17 ի՞նչ է նշանակում NPM
+// 18 Ինչպե՞ս ենք NPM-ը միացնում մեր պրոյեկտին
+/* 
+NPM nshanakum e node package manager,kareli e asel mi gradaran e vory uni shat gorcinqner,
+voroncic menq karox enq ogtval:
+VS terminalum kam ayntex vortex ashxatum enq ayd terminalum katarum enq npm init,patasxanum enq harcerin,
+kam ete chenq uzum patasxanel harcerin npm init -yes, kam npm init -y: 
+*/
+
+//20 Ի՞նչ տարբերություն կա DevDependecy-ի և Dependecy-ի միջև
+/*
+inchic vor mer pojecty kaxvacutyun uni da linum e Dependecy-i  mej, isk ichic vor vor
+kaxvacutyun uni cragravoroxy da linum e DevDependecy-i mej,, DevDependecy i chi poxancvum backendin:
+*/
+// 21 for loop-ով աստղանիշներով console-ում ստանալ լիարժեք՝ կլորանման բան (անկյունները կոտրած),
+//  քառակուսի, եռանկյունի, ուղղանկյուն, երկու քառակուսի իրար կողք և ցանկացած տառ (latin)
+
+function createTrangle(width) {
+	let result = "";
+	for (let i = 1; i <= width; i++) {
+		for (let j = 1; j <= width * 2 - 1; j++) {
+			if (j <= width - i || j >= width + i) {
+				result += " ";
+			} else {
+				result += "*";
+			}
+		}
+		result += "\n";
+	}
+	return result;
+}
+console.log(createTrangle(7));
+function createSquare(width) {
+	let result = "";
+	for (let i = 1; i <= width; i++) {
+		for (let j = 1; j <= width; j++) {
+			result += "*";
+		}
+		result += "\n";
+	}
+	return result;
+}
+console.log(createSquare(5));
+
+//22 ստեղծում ենք փոփոխական, որի մեջ ստեղծում եք սթրինգ 500 բառից բաղկացած պատմություն,
+//ու էդ սթրիգի մեջ օգտագործում եք ${} սրանցից ու մեջը գրում եք տարատեսակ JS կոդեր, որոնք
+//կարող են ձեր պատմության հետ կապ ունենալ, օրինակ (տարեթվեր, անուններ և այլն)
+
+function someText(hero, place, year, city) {
+	let text = `${hero} իր տոհմում այդ անվան երկրորդ կրողը,
+    նախնիններից ժառանգում է կայացած պետություն և մարտունակ բանակ,
+    որի միջոցով էլ կարողանում է իրականացնել հայրենիքի հզորացման և աշխարհակալ
+    տիրակալության ստեղծման իր սկզբունքային ծրագրերը։ Տիգրան Մեծի օրոք ${place}
+    թագավորությունը հասնում է իր հզորության գագաթնակետին։ Պարտության մատնելով 
+    Պարթևական թագավորությանը և ստանալով Սելևկյան գահը՝ Հայքը կարճ ժամանակով
+    դառնում է Առաջավոր Ասիայի հզորագույն պետությունը. Տիգրան Մեծի տերությունը
+    տարածվում էր Կասպից ծովից մինչև Միջերկրական ծով, Կովկասյան լեռներից մինչև
+    Միջագետքի անապատները։ Տիգրանը, սակայն, կորցնում է իր նվաճումների մեծագույն
+    մասը Պարթևական թագավորության և ազդեցիկ Հռոմեական հանրապետության դեմ պայքարում։
+    Օրեցօր բարգավաճող Հայոց թագավորության զարթոնքն արգելակելու նպատակով երբեմնի 
+    հակառակորդները դաշինք են կնքում և շարունակական պատերազմներից հետո՝ մ․թ․ա․ ${year} թվականին,
+    պաշարում հայոց թագավորանիստ-ոստան ${city}։`;
+	return text;
+}
+console.log(someText("Տիգրան Արտաշեսյանը", "Մեծ Հայք", "66", "Արտաշատը"));
+
+//23 ստեղծում եք ֆունկցիա, որը կաշխատի ֆակտորիալ ֆորմուլայով
+
+function factorial(num) {
+	if (num === 0) {
+		return 1;
+	}
+	return num * factorial(num - 1);
+}
+console.log(factorial(5));
+
+//24 ստեղծում եք ֆունկցիա, որը կաշխատի ֆիբոնաչի ֆորմուլայով
+function fibonaci(num) {
+	const arr = [0, 1];
+	for (let i = 1; i < num; i++) {
+		arr.push(arr[i] + arr[i - 1]);
+	}
+
+	return arr;
+}
+console.log(fibonaci(10));
+
+// 25 ստեղծում եք ֆունկցիա, որը որպես արգումենտ ստանում ա ինչ-որ հայտնի շենք/կառույց,
+//  ու վերադարձնում ա էդ շենքի պարամետրերը (բարձրություն, լայնություն, քմ և այլն)
+
+function building(name, height, width, year) {
+	const buildObj = {
+		name: name,
+		height: height,
+		width: width,
+		year: year
+	};
+	return buildObj;
+}
+
+//26 ստեղծում եք ֆունկցիա, որը իր մեջ ստեղծում է մեքենաներ
+// (արգումենտներով), որ ամեն կանչելուց կարողանաք այլ պարամետրեր տալ ու ստանալ տարբեր մեքենաներ
+function carInfo(name, model, color, year) {
+	const carObj = {
+		carName: name,
+		carModel: model,
+		color: color,
+		year: year
+	};
+	return carObj;
+}
+
+// 27 ստեղծում եք 2 զանգված, որտեղ առաջինում պետք է լինեն և բացասական և դրական թվեր,
+//  ու for loop-ի միջոցով զանգվածից հանեք դրական թվերը, իսկ բացասականները դրեք 2րդ դատարկ զանգվածի մեջ
+
+const arr1 = [-2, 5, 6, -7, -11, 15, 24, -6];
+
+function getMinusNumber(arr) {
+	const newArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] < 0) {
+			newArr.push(arr[i]);
+		} else {
+			arr.splice(i, 1);
+			i--;
+		}
+	}
+	return newArr;
+}
+
+// 28 ստեղծում եք օբյեկտ, որտեղ նկարագրում եք մարդու, որը ունի ընկերներ, ստատուսներ,
+//  սև ցուցակի մեջ մարդիկ, լայքեր, էլ. հասցե, հեռախոս, գաղտնաբառ և այլն (նմանեցնել Ֆեյսբուքին)
+
+const person = {
+	firstName: "Jack",
+	lastName: "Adams",
+	addres: "New York 11/3",
+	phone: "+9 999 999 999",
+	email: "jack-adams@gamil.com",
+	socialMedia: true,
+	password: "jack1990",
+	friends: ["Mike", "John", "Mary", "Sara", "Klara", "Jordan", "Stephan"],
+	blackList: ["Gloria", "Samanta", "Johnson"],
+	statues: "Dreams don't work unless you do",
+	actives: [
+		{ name: "firstPost", like: 350, share: 100, dislike: 150, comments: 100 },
+		{ name: "firstPost", like: 200, share: 50, dislike: 20, comments: 60 },
+		{ name: "firstPost", like: 50, share: 0, dislike: 0, comments: 3 },
+		{ name: "firstPost", like: 65, share: 20, dislike: 5, comments: 10 },
+		{ name: "firstPost", like: 5, share: 0, dislike: 0, comments: 1 }
+	],
+	profileSecurity: "medium",
+	profileOpen: true
+};
+
+// 29 ստեղծում եք զանգված, ու մեջը 10 հատ օբյեկտ,
+//  ամեն օբյեկտում ձեր նախընտրած մարդկանց մասին մի քանի ինֆորմացիա՝ անուն ազգանուն, տարիք,
+//  մասնագիտություն ու ազգություն, հետո loop-ի միջոցով էդ օբյետկտերի մարդկանց անունները տպում եք կոնսոլում
+
+const armenianHeroes = [
+	{
+		name: "Monte Melkonian",
+		givenHero: "20 September 1996",
+		occupation: "military commander"
+	},
+	{
+		name: "Vazgen I",
+		givenHero: "28 July 1994",
+		occupation: "Catholicos of All Armenians"
+	},
+	{
+		name: "Charles Aznavour",
+		givenHero: "27 May 2004",
+		occupation: "singer, philanthropist"
+	},
+	{
+		name: "Arkady Ter-Tatevosyan",
+		givenHero: "8 May 2021",
+		occupation: "military commander"
+	},
+	{
+		name: "Viktor Hambardzumyan",
+		givenHero: "11 October 1994",
+		occupation: "scientist"
+	},
+	{
+		name: "Vazgen Sargsyan",
+		givenHero: "27 December 1999",
+		occupation: "politician"
+	},
+	{
+		name: "Kirk Kerkorian",
+		givenHero: "27 May 2004",
+		occupation: "businessman, philanthropist"
+	}
+];
+function getHeroesNames(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		console.log(arr[i].name);
+	}
+}
+getHeroesNames(armenianHeroes);
+
+// 30 ստեղծում եք Math անունով օբյեկտ,
+//  որի մեջ մեթոդներով ստեղծում ենք այն մաթեմատիկական ալգորիթմներ (արմատ, քառակուսի և այլն)
+
+const myMath = {
+	square(num) {
+		return num * num;
+	},
+	backSquare(num) {
+		for (let i = 0; i <= num / 2; i++) {
+			if (i * i == num) {
+				return i;
+			}
+		}
+	},
+	round(num) {
+		let numArr = num.toString();
+		let roundNum = numArr.slice(0, numArr.indexOf("."));
+		return +roundNum;
+	}
+};
+
+console.log(myMath.round(15.2));
+
+// 31
+/*
+typeof true "Boolean"
+typeof false "Boolean"
+typeof function () {} "function"
+typeof null "object"
+typeof [] "object"
+typeof {} "object"
+typeof " " "string"
+typeof 0  "number"
+typeof 1 "number"
+typeof NaN "number"
+typeof undefined "undefined"
+typeof Object "function"
+typeof Object() "object"
+typeof Array "function"
+typeof Array() "object"
+typeof Boolean "function"
+typeof Bollean() "boolean"
+*/
+
+/*
+NaN || 2 || undefined;  --2
+
+NaN && 2 && undefined; --NaN
+
+1 && 2 && 3;  --3
+
+(!1 && 2) || !3; --!3
+
+25 || (null && !3); --25
+
+NaN || null || !3 || undefined || 5; --5
+
+NaN || (null && !3 && undefined) || 5; ----5
+
+(5 === 5 && 3 > 1) || 5; --true
+
+(10 <= 8 && !9) || 10 > 5; --true
+
+*/
+
+// 41
+// ........................... false...........   true
+// const a = true, b = false; console.log(!a) console.log(!b)
+
+// 42
+// .....................................true...................false...........true.............true
+// const a = true, b = false, c = 4; console.log(a || b); console.log(b || b); console.log((c>2) || (c<2));
+
+// 43
+// ............................................true...................false...................false
+// const a = true, b = false; const c = 4; console.log(a && a); console.log(a && b); console.log((c > 2) && (c < 2));
+
+// 44
+// ...................................false..............true.......................false
+// const a = 2, b = 'hello'; console.log(a !== 2); console.log(a !== '2'); console.log(b !== 'Hello');
+
+// 45
+// ..............true..........................true.......................true..........................false
+// console.log( true || true ); console.log( false || true ); console.log( true || false ); console.log( false || false );
+
+// 46
+// ...........................  1 hat "FALSE"
+// Ինչ կստանանք կոնսոլի մեջ let x = 4; do { console.log("FALSE"); x++; } while (x > 4 && x > 6)
+
+// 47
+// ..............1......................1...............................1
+// console.log( 1 || 0 ); console.log( null || 1 ); console.log( null || 0 || 1 );
+
+// ......................0.....................................3
+// console.log( undefined || null || 0 ); console.log( null || 2 && 3 || 4 )
+
+// 48
+// ..............kstananq first.......................____________________........................___________________
+// if (-1 || 0) console.log( 'first' ); if (-1 && 0) console.log( 'second' ); if (null || -1 && 1) console.log( 'third' );
+
+// 49
+// console.log(!undefined); --true
+//  console.log(!null); --true
+//  console.log(!20); --false
+// console.log(!0); --true
+//  console.log(!NaN); --true
+// console.log(!{});  --false
+// console.log(!''); --true
+//  console.log(!'OK'); --false
+//  console.log(!false); --true
+//  console.log(!true); --false
+
+// 50
+// .......................true...................false.................true...............false......true
+// const a = 5, b = 10; (a != b) && (a < b); (a > b) || (a == b); (a < b) || (a == b); !(a < b); !(a > b);
